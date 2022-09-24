1437a631a83199e1269b903d0c802c9614ea7b11
Kamil Dróżdż <kamil12x@wp.pl>
2022-09-24 21:30:26 +0200

    adds RWD and style switcher

---
+++ app.js
@@ -0,0 +1,12 @@
+const textColor = getComputedStyle(document.body).getPropertyValue('--skin-color');
+const resetBtn = document.getElementById('reset');
+const textColorPicker = document.querySelector('.color-picker');
+const themeOptions = [textColorPicker];
+
+themeOptions.forEach(function (option) {
+	option.addEventListener('change', event => {
+		document.body.style.setProperty(option.dataset.themeValue, event.target.value);
+	});
+});
+
+resetBtn.addEventListener('click', () => document.body.style.setProperty('--skin-color', '#FF0000'));
