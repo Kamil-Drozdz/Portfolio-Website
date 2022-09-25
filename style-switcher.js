8fa84cca650a0646f0c17575aa7a70a23b6be228
Kamil_Drozdz <Kamil12x@wp.pl>
2022-09-25 17:22:23 +0200

    adds theme night/light, active navbar, typing animation

--- style-switcher.js
+++
@@ -1,6 +0,0 @@
-/*style switcher */
-
-const styleSwiitcherToggle = document.querySelector('.style-switcher-toggler');
-styleSwiitcherToggle.addEventListener('click', () => {
-	document.querySelector('.style-switcher').classList.toggle('open');
-});
